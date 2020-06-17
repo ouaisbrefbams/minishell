4cda5fc0596012647fe5390c38ae0708b40f42dc
nass1pro <nass1pro@gmail.com>
2020-06-17 14:53:17 +0200

    Added Builtin cd and export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: charles <charles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/01 17:11:34 by charles           #+#    #+#             */
-/*   Updated: 2020/06/17 13:47:18 by nahaddac         ###   ########.fr       */
+/*   Updated: 2020/06/18 13:50:47 by charles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,25 @@
 
 // modify existing
 // set with no string without '='
+// TODO: multiple exported variable (e.g export A=a B=b C=c)
 
 #include "minishell.h"
 
-int	builtin_export(char **argv)
+int	builtin_export(char **argv, t_env env)
 {
 	char	*temp;
 	size_t  i;
 
+	(void)env;
 	if (argv[1] == NULL)
 		return (4);
-	if(isdigit(argv[1][0]))
+	if(ft_isdigit(argv[1][0]))
 		return(0);
 	i = 0;
 	temp = argv[1];
 	while(temp[i] != '\0')
 	{
-		if(temp[i] == ' ' || isalnum(temp[i]) == 0)
+		if(temp[i] == ' ' || ft_isalnum(temp[i]) == 0)
 			return(2);
 		if (temp[i] == '=')
 		{
