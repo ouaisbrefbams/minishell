70459a3ab97b13df9bf829e29ed91becfba1ea03
nass1pro <nass1pro@gmail.com>
2020-06-18 13:19:21 +0200

    Change parse

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: cacharle <cacharle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 09:00:00 by cacharle          #+#    #+#             */
-/*   Updated: 2020/06/17 18:03:33 by nahaddac         ###   ########.fr       */
+/*   Updated: 2020/06/18 12:40:22 by nahaddac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,20 @@
 ** parse.c
 */
 
+
 t_ret				*parse(t_ftlst *input);
-t_ret				*parse_c(t_ftlst *input);
+t_ret		        *parse_op(t_ftlst *input)
+t_ret		        *parse_expr(t_ftlst *input);
+t_ret               *parse_cmd(t_ftlst *input);
+
+// utils
+t_ret			    *ret_wrap_ast(t_ast *ast, t_ftlst *rest);
+t_ftlst				*push_token(t_ftlst **tokens, t_token *pushed);
+
+
+
 t_ast 				*push_cmd(t_ast *ast, t_ftlst *ret);
 t_ast 				*push_redir(t_ast *ast, t_ftlst *rest);
-
 int                 parse_cmd_str_true_false(enum e_token_tag tag);
 int                 parse_redir_true_false(enum e_token_tag tag);
 
