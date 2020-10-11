8224c28be6d9229cf3e4b2c21f4ec81a4083caa8
Charles Cabergs <me@cacharle.xyz>
2020-10-11 14:06:26 +0200

    Norming libft

--- src/preprocess/preprocess.c
+++ src/preprocess/preprocess.c
@@ -6,7 +6,7 @@
 /*   By: charles <charles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/03 08:58:49 by charles           #+#    #+#             */
-/*   Updated: 2020/10/10 10:56:41 by cacharle         ###   ########.fr       */
+/*   Updated: 2020/10/10 14:15:44 by cacharle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 ** \return        The resulting list of tokens
 */
 
-t_tok_lst			*st_stick_tokens(t_tok_lst *tokens)
+static t_tok_lst	*st_stick_tokens(t_tok_lst *tokens)
 {
 	t_tok_lst	*curr;
 	t_tok_lst	*tmp;
@@ -35,7 +35,7 @@ t_tok_lst			*st_stick_tokens(t_tok_lst *tokens)
 			curr->tag = curr->next->tag;
 			ft_lstdelone((t_ftlst *)curr->next, free);
 			curr->next = tmp;
-			continue;
+			continue ;
 		}
 		curr = curr->next;
 	}
@@ -49,7 +49,7 @@ t_tok_lst			*st_stick_tokens(t_tok_lst *tokens)
 ** \note          tokens is destroyed
 */
 
-char				**st_tokens_to_argv(t_tok_lst **tokens)
+static char			**st_tokens_to_argv(t_tok_lst **tokens)
 {
 	char		**ret;
 	size_t		i;
@@ -78,11 +78,11 @@ char				**st_tokens_to_argv(t_tok_lst **tokens)
 **             false otherwise
 */
 
-bool				escape(char *str, enum e_tok tag)
+static bool			st_escape(char *str, enum e_tok tag)
 {
 	if (str[0] == '\\' &&
-		(tag & TAG_STR || ((tag & TAG_STR_DOUBLE)
-							&& ft_strchr("\\\"$", str[1]))))
+		(tag & TAG_STR ||
+			((tag & TAG_STR_DOUBLE) && ft_strchr("\\\"$", str[1]))))
 	{
 		ft_memmove(str, str + 1, ft_strlen(str + 1) + 1);
 		return (true);
@@ -113,11 +113,11 @@ char				**preprocess(t_tok_lst **tokens, t_env env)
 		{
 			while ((str = curr->content) != NULL && str[++i] != '\0')
 			{
-				if (escape(str + i, curr->tag))
-					continue;
+				if (st_escape(str + i, curr->tag))
+					continue ;
 				if (str[i] == '$')
-					i = interpolate((void*[2]){str, &curr},
-							i, prev_tag, env) - 1;
+					i = interpolate(
+							(void *[2]){ str, &curr }, i, prev_tag, env) - 1;
 			}
 		}
 		prev_tag = curr->tag;
