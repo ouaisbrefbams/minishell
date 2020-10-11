a4ceb5974d1b7dcdd12cc81b7eb07893ea16c8ad
Charles Cabergs <me@cacharle.xyz>
2020-10-11 14:54:04 +0200

    Fixing read of uninitialized value error in bultin/exit.c

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: charles <charles.cabergs@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/01 17:10:16 by charles           #+#    #+#             */
-/*   Updated: 2020/09/28 10:51:42 by cacharle         ###   ########.fr       */
+/*   Updated: 2020/10/11 14:27:03 by cacharle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,9 @@ int	builtin_exit(char **argv, t_env env)
 	{
 		errno = 0;
 		status = ft_strtol(argv[1], &after, 10);
-		while (ft_isblank(*after))
+		while (after != NULL && ft_isblank(*after))
 			after++;
-		if (*after != '\0' || errno == ERANGE)
+		if (errno == ERANGE || *after != '\0')
 		{
 			errorf("exit: %s: numeric argument required\n", argv[1]);
 			return (255);
